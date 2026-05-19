b3545fbcf244891ef5a154bae81e21a9a587f252
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2026-05-19 21:11:33 +0530

    add quit command

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # pop3
 
 implementation of pop3 server from [rfc-1938](https://www.ietf.org/rfc/rfc1939.txt)
+
+## setup
+
+start the server by runing
+```
+go run .
+```
+to test the server 
+```
+nc -C localhost 5000
+```
+you can change port number if you want in main.go file
--- main.go
+++ main.go
@@ -1,39 +1,72 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
 	"strings"
+	"time"
 )
+
+const (
+	QUIT string = "QUIT"
+	STAT string = "STAT"
+	LIST string = "LIST"
+	RETR string = "RETR"
+	DELE string = "DELE"
+	NOOP string = "NOOP"
+	RSET string = "RSET"
+	TOP  string = "TOP"
+	UIDL string = "UIDL"
+	USER string = "USER"
+	PASS string = "PASS"
+)
+
+func handleCommand(conn net.Conn, cmd string, args []string) {
+	cmd = strings.ToUpper(cmd)
+	switch cmd {
+	case QUIT:
+		commandQUIT(conn)
+	}
+}
+
+func commandQUIT(conn net.Conn) {
+	conn.Write([]byte("+OK POP3 server signing off\r\n"))
+}
 
 func handleConn(conn net.Conn, err error) {
 	if err != nil {
 		conn.Write([]byte("-ERR connection falied\r\n"))
-		conn.Close()
 		return
 	}
+	defer conn.Close()
+	conn.SetDeadline(time.Now().Add(10 * time.Minute))
 	conn.Write([]byte("+OK POP3 server ready\r\n"))
-	packets := make([]byte, 1024)
+	packets := []byte{}
 	tmp := make([]byte, 1024)
 	for {
-		_, err := conn.Read(tmp)
+		n, err := conn.Read(tmp)
 		if err != nil {
-			conn.Write([]byte("-ERR connection failed\r\n"))
-			conn.Close()
+			if err != io.EOF {
+				conn.Write([]byte("-ERR read failed\r\n"))
+			} else {
+				commandQUIT(conn)
+			}
 			return
 		}
-		packets = append(packets, tmp...)
-		if strings.Contains(string(packets), "QUIT") {
-			conn.Write([]byte("+OK POP3 server signing off\r\n"))
-			conn.Close()
-			return
+		packets = append(packets, tmp[:n]...)
+		if strings.HasSuffix(string(packets), "\r\n") {
+			cmd := string(packets)
+			cmd = cmd[:len(cmd)-2]
+			packets = []byte{}
+			args := strings.Split(cmd, " ")
+			handleCommand(conn, args[0], args[1:])
 		}
-		packets = make([]byte, 1024)
 	}
 }
 
 func main() {
-	ln, err := net.Listen("tcp", ":5000")
+	ln, err := net.Listen("tcp", "0.0.0.0:5000")
 	if err != nil {
 		log.Fatal(err)
 	}
