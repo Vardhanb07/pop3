6fff6054ecb0e24c02ae5ac503ef4eb5b0d96275
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2026-05-20 12:33:42 +0530

    add user command

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+	"path"
 	"strings"
 	"time"
 )
@@ -22,26 +25,101 @@ const (
 	PASS string = "PASS"
 )
 
-func handleCommand(conn net.Conn, cmd string, args []string) {
+// GREET -> AUTH -> TRANS -> UPDATE
+//
+// consider maildrop for this project as ~/.pop3/test.com/test for test@test.com
+// the directory ~/.pop3/test.com contains test, .hash
+// .hash file contains hash of user and pass
+// a test user test@test.com with pass pass
+const (
+	GREET  string = "GREET"
+	AUTH   string = "AUTH"
+	TRANS  string = "TRANS"
+	UPDATE string = "UPDATE"
+)
+
+type ClientSession struct {
+	State       string
+	Conn        net.Conn
+	Name        string
+	Pass        string
+	MailBoxPath string
+}
+
+func newClientSession(conn net.Conn) *ClientSession {
+	return &ClientSession{
+		State: GREET,
+		Conn:  conn,
+	}
+}
+
+func handleCommand(session *ClientSession, cmd string, args []string) {
 	cmd = strings.ToUpper(cmd)
 	switch cmd {
 	case QUIT:
-		commandQUIT(conn)
+		commandQUIT(session)
+	case USER:
+		commandUSER(session, args)
+	case PASS:
+		commandPASS(session)
 	}
 }
 
-func commandQUIT(conn net.Conn) {
+func commandQUIT(session *ClientSession) {
+	conn := session.Conn
 	conn.Write([]byte("+OK POP3 server signing off\r\n"))
 }
 
-func handleConn(conn net.Conn, err error) {
-	if err != nil {
-		conn.Write([]byte("-ERR connection falied\r\n"))
+// USER username@domain
+// mailbox -> /.pop3/domain/username
+func commandUSER(session *ClientSession, args []string) {
+	if session.State != AUTH {
+		session.Conn.Write([]byte("-ERR command not available\r\n"))
 		return
 	}
+	if len(args) == 0 {
+		session.Conn.Write([]byte("-ERR argument incomplete\r\n"))
+		return
+	}
+	s := strings.Split(args[0], "@")
+	if len(s) < 2 {
+		session.Conn.Write([]byte("-ERR argument incomplete\r\n"))
+		return
+	}
+	// name -> s[0], domain -> s[1]
+	hdir, err := os.UserHomeDir()
+	if err != nil {
+		session.Conn.Write([]byte("-ERR mailbox read failed\r\n"))
+	}
+	mailbox := path.Join(hdir, ".pop3", s[1], s[0])
+	// check for the file ~/.pop3/domain/name
+	_, err = os.Open(mailbox)
+	if os.IsNotExist(err) {
+		s := fmt.Sprintf("-ERR never heard of %s\r\n", args[0])
+		session.Conn.Write([]byte(s))
+		return
+	}
+	session.MailBoxPath = mailbox
+	session.Name = s[0]
+}
+
+func commandPASS(session *ClientSession) {
+	if session.State != AUTH {
+		session.Conn.Write([]byte("-ERR command not available\r\n"))
+		return
+	}
+	if len(session.Name) == 0 {
+		session.Conn.Write([]byte("-ERR invalid password\r\n"))
+		return
+	}
+}
+
+func handleConn(session *ClientSession) {
+	conn := session.Conn
 	defer conn.Close()
 	conn.SetDeadline(time.Now().Add(10 * time.Minute))
 	conn.Write([]byte("+OK POP3 server ready\r\n"))
+	session.State = AUTH
 	packets := []byte{}
 	tmp := make([]byte, 1024)
 	for {
@@ -50,7 +128,7 @@ func handleConn(conn net.Conn, err error) {
 			if err != io.EOF {
 				conn.Write([]byte("-ERR read failed\r\n"))
 			} else {
-				commandQUIT(conn)
+				commandQUIT(session)
 			}
 			return
 		}
@@ -60,7 +138,7 @@ func handleConn(conn net.Conn, err error) {
 			cmd = cmd[:len(cmd)-2]
 			packets = []byte{}
 			args := strings.Split(cmd, " ")
-			handleCommand(conn, args[0], args[1:])
+			handleCommand(session, args[0], args[1:])
 		}
 	}
 }
@@ -70,8 +148,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("server listening at port 5000")
 	for {
 		conn, err := ln.Accept()
-		go handleConn(conn, err)
+		if err != nil {
+			conn.Write([]byte("-ERR connection failed\r\n"))
+			return
+		}
+		session := newClientSession(conn)
+		go handleConn(session)
 	}
 }
